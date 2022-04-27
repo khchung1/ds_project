c978fbbc689c7cccc58bab85d83b064a838072a3
khchung1 <khchung87@gmail.com>
2022-04-27 19:52:09 +0800

    retry3

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ from logregression import logistic_regression
 app = Flask(__name__)
 
 
-
 @app.route('/')
 def home():
 
     return render_template("index.html")
+
 
 @app.route('/predict', methods=['POST'])
 def predict():
